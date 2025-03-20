4dca4188976553162d7937b2e1e7a0d227beec85
aelsi2 <eliseevandrey2005@gmail.com>
2025-03-20 15:05:13 +0700

    refactor draw_line

--- src/lines.c
+++ src/lines.c
@@ -7,28 +7,33 @@ void draw_line(point_t start, point_t end, color_t color){
     int x1 = get_x(end);
     int y1 = get_y(end);
 
-    if (x0 == x1)
+    if (x0 == x1) {
         draw_vertical_line(start, end, color);
-    else if (y0 == y1)
+    } else if (y0 == y1) {
         draw_horizontal_line(start, end, color);
-    else {
-        int deltax = abs(x1 - x0);
-        int deltay = -abs(y1 - y0);
-        int dirx = x0 < x1 ? 1 : -1;
-        int diry = y0 < y1 ? 1 : -1;
-        int error = deltax + deltay;
-        while(1){
+    } else {
+        int delta_x = abs(x1 - x0);
+        int delta_y = -abs(y1 - y0);
+        int dir_x = sign(x1 - x0);
+        int dir_y = sign(y1 - y0);
+        int double_dx = delta_x * 2;
+        int double_dy = delta_y * 2;
+        int error = double_dx + double_dy;
+        while (1) {
             draw_pixel(point(x0, y0), color);
-            int double_error = error * 2;
-            if(double_error >= deltay){
-                if(x0 == x1) break;
-                error += deltay;
-                x0 += dirx;
+            if (error >= delta_y) {
+                if (x0 == x1) { 
+                    break;
+                }
+                error += double_dy;
+                x0 += dir_x;
             }
-            if(double_error <= deltax){
-                if(y0 == y1) break;
-                error += deltax;
-                y0 += diry;
+            if (error <= delta_x){
+                if (y0 == y1) { 
+                    break;
+                }
+                error += double_dx;
+                y0 += dir_y;
             }
         }
     }
--- src/math.h
+++ src/math.h
@@ -3,3 +3,4 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define abs(a) ((a) < 0 ? -(a) : (a))
+#define sign(a) ((a) < 0 ? -1 : (a) > 0)
