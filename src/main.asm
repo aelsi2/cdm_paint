5c0507645ba13fd60297e4ca680af06acda543a9
aelsi2 <eliseevandrey2005@gmail.com>
2025-03-21 13:31:31 +0700

    make controller inputs more reliable
    Though they now make the processor die sometimes. IDK why

--- src/main.asm
+++ src/main.asm
@@ -1,14 +1,17 @@
 asect 0
 asm_main: ext
-asm_handler: ext
+asm_handler1: ext
+asm_handler2: ext
+asm_handler3: ext
+asm_handler4: ext
 asm_input: ext
 
 # IVT
 dc asm_main, 0b1000000000000000
-dc asm_handler, 0
-dc asm_handler, 0
-dc asm_handler, 0
-dc asm_handler, 0
+dc asm_handler1, 0
+dc asm_handler2, 0
+dc asm_handler3, 0
+dc asm_handler4, 0
 dc asm_input, 0
 align 0x80
 
@@ -20,7 +23,20 @@ asm_main>
 jsr main
 halt
 
-asm_handler>
+asm_handler1>
+ldi r0, 0xDED1
+halt
+
+asm_handler2>
+ldi r0, 0xDED2
+halt
+
+asm_handler3>
+ldi r0, 0xDED3
+halt
+
+asm_handler4>
+ldi r0, 0xDED4
 halt
 
 asm_input>
--- src/main.c
+++ src/main.c
@@ -2,6 +2,10 @@
 
 #pragma clang diagnostic ignored "-Wmain-return-type"
 void main() {
+    draw_horizontal_line(point(0, 4), point(7, 4), COLOR_WHITE);
+    draw_vertical_line(point(8, 0), point(8, 4), COLOR_WHITE);
+    update_screen();
+
     while(1);
     clear(COLOR_WHITE);
     update_screen();
@@ -17,8 +21,16 @@ void main() {
     update_screen();
 }
 
-extern char joy;
+extern volatile const unsigned char joy;
 void handle_input() {
-    draw_pixel(joy, COLOR_WHITE);
+    unsigned char joy_new = joy;
+    draw_pixel(point(5, 1), (joy_new & 1) != 0);
+    draw_pixel(point(4, 1), (joy_new & 2) != 0);
+    draw_pixel(point(1, 2), (joy_new & 4) != 0);
+    draw_pixel(point(1, 0), (joy_new & 8) != 0);
+    draw_pixel(point(2, 1), (joy_new & 16) != 0);
+    draw_pixel(point(0, 1), (joy_new & 32) != 0);
+    draw_pixel(point(6, 0), (joy_new & 64) != 0);
+    draw_pixel(point(0, 0), (joy_new & 128) != 0);
     update_screen();
 }
