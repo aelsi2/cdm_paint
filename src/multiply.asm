d18ca06dfe1979a9c99bf91bfc92924ea08d1638
aelsi2 <eliseevandrey2005@gmail.com>
2025-04-08 11:09:25 +0700

    optimize multiplication

--- src/multiply.asm
+++ src/multiply.asm
@@ -2,39 +2,17 @@ rsect multiply
 
 multiply>
 ldi r2, 0
-save r4
-ldi r4, 1
-if
-    tst r0
-is mi
-    neg r0
-    neg r4
-fi
-if
-    tst r1
-is mi
-    neg r1
-    neg r4
-fi
 while
 tst r1
-stays ne
+stays nz
     if
-        ldi r3, 1
-        and r1,r3
-    is ne
+        shr r1
+    is cs
         add r0,r2
     fi
     shl r0
-    shr r1
 wend
-if 
-    tst r4
-is mi
-    neg r2
-fi
-restore
 move r2,r0
-
 rts
+
 end.
