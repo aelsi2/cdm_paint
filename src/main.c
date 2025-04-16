11644df556c1736e9524392ddad28bbbc7606060
CrocoZaka <CrocoZaka@gmail.com>
2025-04-16 13:32:50 +0700

    add video adapter + error cursor

--- src/main.c
+++ src/main.c
@@ -5,8 +5,10 @@
 #include "screen.h"
 #include "drawing/internal.h"
 
-int cursor_x = 0;
-int cursor_y = 0;
+int cursor_x = 15;
+int cursor_y = 15;
+int counter = 0;
+buttons_t joy_new = 0;
 
 void update_screen() {
     screen_write_range(dr_context->frame_buffer, dr_context->dirty_start, dr_context->dirty_end);
@@ -36,26 +38,52 @@ void main() {
     static dr_context_t ctx;
     dr_context = &ctx;
     dr_reset_dirty();
-    dr_clear(COLOR_WHITE);
-
-    update_screen();
-    dr_draw_filled_rect(pt(3, 3), pt(28, 28), COLOR_BLACK);
-    update_screen();
-    /*dr_draw_line(pt(5, 7), pt(11, 5), COLOR_WHITE);
-    update_screen();
-    dr_draw_line(pt(20, 26), pt(26, 24), COLOR_WHITE);
-    update_screen();
-    dr_draw_filled_rect(pt(15, 15), pt(16, 16), COLOR_WHITE);
-    update_screen();
-    dr_draw_outline_rect(pt(13, 13), pt(18, 18), COLOR_WHITE);
-    update_screen();
-    dr_draw_outline_ellipse(pt(15, 15), 7, 7, COLOR_WHITE);
-    update_screen();*/
-    while(1);
+    while(1){
+        if(joy_new == 0){
+            counter = 50;
+            show_cursor(cursor_x, cursor_y);
+            restore_row(cursor_y);
+        }
+        if(counter == 0){
+            if(joy_new & BTN_RIGHT){
+                cursor_x++;
+            }
+            if(joy_new & BTN_LEFT){
+                cursor_x--;
+            }
+            if(joy_new & BTN_DOWN){
+                restore_row(cursor_y);
+                cursor_y++;
+            }
+            if(joy_new & BTN_UP){
+                restore_row(cursor_y);
+                cursor_y--;
+            }
+            if(cursor_x >= SCREEN_WIDTH){
+                cursor_x -= SCREEN_WIDTH;
+            }
+            if(cursor_x < 0){
+                cursor_x += SCREEN_WIDTH;
+            }
+            if(cursor_y >= SCREEN_HEIGHT){
+                restore_row(cursor_y);
+                cursor_y -= SCREEN_HEIGHT;
+            }
+            if(cursor_y < 0){
+                restore_row(cursor_y);
+                cursor_y += SCREEN_HEIGHT;
+            }
+            show_cursor(cursor_x, cursor_y);
+            counter+=3;
+        }
+        if(counter > 0){
+            counter--;
+        }
+    };
 }
 
 void handle_input() {
-    buttons_t joy_new = joy;
+    joy_new = joy;
     if(joy_new & BTN_RIGHT){
         cursor_x++;
     }
@@ -83,4 +111,5 @@ void handle_input() {
         cursor_y += SCREEN_HEIGHT;
     }
     show_cursor(cursor_x, cursor_y);
+    counter = 50;
 }
